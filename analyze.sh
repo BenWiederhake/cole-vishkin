97879a7dcfd226018d77fbdb633ba216f0432e36
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2015-11-04 14:16:04 +0100

    Ease formatting for analysis

--- analyze.sh
+++ analyze.sh
@@ -4,7 +4,7 @@ for cpus in $(seq 1 4)
 do
 	for i in $(seq 1 10)
 	do
-		echo @./cv-fast --init-pattern xorshift128plus --init-seed $i --file-out /dev/null --cpus $cpus
+		echo @./cv-fast --init-pattern xorshift128plus --init-seed $i --file-out /dev/null --cpus $cpus --format tdl
 	done
 	echo "#"
 done
